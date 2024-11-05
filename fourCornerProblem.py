b5b71450eff2f3040a0b204e433a4429e4a8fc86
BaeSooMin <102912063+sumsum07@users.noreply.github.com>
2024-11-04 20:20:29 -0600

    complete A*

--- fourCornerProblem.py
+++ fourCornerProblem.py
@@ -1,46 +1,42 @@
+import itertools
+
 class Problem:
-    def __init__ (self, mazeFile):
+    def __init__(self, mazeFile):
         self.xStep = 40
         self.yStep = 40
-        self.readMaze (mazeFile)
+        self.readMaze(mazeFile)
+        self.compute_distances() 
 
-    def readMaze (self, mazeFile):
+    def readMaze(self, mazeFile):
         self.walls = []
         self.pacman = 0
         self.food = []
         self.xMax = 0
         self.yMax = 0
         
-        f = open (mazeFile, 'r')
-        if f:
+        with open(mazeFile, 'r') as f:
             y = 0
             while True:
-                s = list (f.readline ())
-                #print (s)
-                
+                s = list(f.readline())
                 if s == []: break
                 if s[-1] == '\n': s.pop()
-                x=0
+                x = 0
                 for k in s:
-                    if k == '*': self.walls.append ((x, y))
+                    if k == '*': self.walls.append((x, y))
                     if k == 'P': self.pacman = (x, y)
-                    if k == '.': self.food.append ((x, y))
+                    if k == '.': self.food.append((x, y))
                     x += 1
                 y += 1
-                self.xMax = max (self.xMax, x)
-            self.yMax = y            
-            return True
-        else:
-            print ('File not found')
-        return False
-
-    def startState (self):
+                self.xMax = max(self.xMax, x)
+            self.yMax = y
+            
+    def startState(self):
         return (self.pacman, self.food)
 
-    def isGoal (self, node):
+    def isGoal(self, node):
         return node[1] == []
 
-    def transition (self, node):
+    def transition(self, node):
         x, y = node[0]
         newState = []
 
@@ -51,34 +47,23 @@ class Problem:
 
         moves = [(move, a) for move, a in potential_moves if 0 <= move[0] < self.xMax and
                  0 <= move[1] < self.yMax and move not in self.walls]
-        newStates = []
         
         for k, action in moves:
             remainingDots = node[1].copy()
             if k in remainingDots:
                 remainingDots.remove(k)
-            newState.append (((k, remainingDots), action, 1))
+            newState.append(((k, remainingDots), action, 1))
 
         return newState
 
-    def compute_distances (self):
+    def compute_distances(self):
         self.dist = {}
-        for f in self.food:
-            self.dist[(self.pacman, f)] = self.BFS(self.pacman, f)
-            for d in self.food:
-                if d == f: continue
-                plan1 = self.BFS(f, d)
-                self.dist[(f, d)] = plan1.copy()
-                # reverse plan1:
-                plan2=[]
-                for k in plan1:
-                    if k == 'U': plan2.append('D')
-                    if k == 'D': plan2.append('U')
-                    if k == 'L': plan2.append('R')
-                    if k == 'R': plan2.append('L')
-                plan2.reverse()
-                self.dist[(d, f)] = plan2
-
+        
+        positions = [self.pacman] + self.food
+        for pos1, pos2 in itertools.combinations(positions, 2):
+            distance = self.BFS(pos1, pos2)
+            self.dist[(pos1, pos2)] = distance
+            self.dist[(pos2, pos1)] = distance  
     def BFS(self, start_pos, target_pos):
         def construct_path(node, visited):
             path = []
@@ -86,9 +71,9 @@ class Problem:
                 node, a = visited[node]
                 if node != None: path = [a] + path
             return path
+
         def transition(node):
             x, y = node
-
             potential_moves = [((x+1, y), 'R'),
                                ((x-1, y), 'L'),
                                ((x, y+1), 'D'),
@@ -104,33 +89,45 @@ class Problem:
             node, parent, action = frontier.pop(0)
             if node in visited: continue
             visited[node] = (parent, action)
-            if node == target_pos: return construct_path(node, visited)
+            if node == target_pos: return len(construct_path(node, visited))
             neighbors = transition(node)
             for n, a in neighbors:
-                frontier.append ((n, node, a))
-        return None
-                
+                frontier.append((n, node, a))
+        return float('inf')
 
     def nextStates(self, node):
-        # method to return the next state with the cost.
-        # Return value is a tuple of the form:
-        #   (cost, (pacman_pos, remaining_dots), actions)
-        # The actions are the plan to move from pacman_pos to one
-        # of the dot in the remaining_dots
         neighbors = []
         for f in node[1]:
-            #plan = self.BFS(node[0], f)
-            
-            plan = self.dist[(node[0], f)]
+            plan_length = self.dist[(node[0], f)]
             dots = node[1].copy()
             dots.remove(f)
-            #neighbors.append ((len(plan), (f, dots), plan))
-            neighbors.append ((len(plan), (f, dots), plan))
+            neighbors.append((plan_length, (f, dots), plan_length))
         return neighbors 
 
     def h(self, state):
-        # heuristic method that computes the minimum manhattan distance
-        # from pacman position to every remaining dot.
-        if state[1] == []: return 0
-        # Complete your code here
-        
+        pacman_pos = state[0]
+        remaining_foods = state[1]
+
+        if not remaining_foods:
+            return 0
+
+        mst = {food: False for food in remaining_foods}
+        sumSteps = 0
+        current_pos = pacman_pos
+
+        while not all(mst.values()):
+            min_distance = float('inf')
+            closest_food = None
+            for food in remaining_foods:
+                if not mst[food]:
+                    distance = self.dist.get((current_pos, food), float('inf'))
+                    if distance < min_distance:
+                        min_distance = distance
+                        closest_food = food
+            
+            if closest_food:
+                mst[closest_food] = True
+                sumSteps += min_distance
+                current_pos = closest_food
+
+        return sumSteps
--- fourCorners.py
+++ fourCorners.py
@@ -12,8 +12,10 @@ def construct_path(node, visited):
         food = tuple(node[1])
         node, action = visited[(node[0], food)]
         if action != None:
-                if type(action) == list: path = action + path
-                else: path = [action] + path
+            if type(action) == list:
+                path = action + path
+            else:
+                path = [action] + path
     return path
 
 def bfs(p):
@@ -27,106 +29,71 @@ def bfs(p):
         if (node[0], food) in visited: continue
         visited[(node[0], food)] = (parent, action)
         if p.isGoal(node):
-            print ('BFS number of nodes explored: ', count)
+            print('BFS number of nodes explored: ', count)
             return construct_path(node, visited)
         count += 1
         neighbors = p.transition(node)
         for n, a, c in neighbors:
             food = tuple(n[1])
             if (n, node, a) not in frontier and (n[0], food) not in visited:
-                frontier.append ((n, node, a))
+                frontier.append((n, node, a))
     return []
 
 def ucs(p):
-    # define priority queue: pq
     pq = [(0, p.startState(), None, None)]
     visited = {}
     count = 0
     while pq:
         gCost, node, parent, action = heapq.heappop(pq)
-
-        # Check if node is visited
         food = tuple(node[1])
         if (node[0], food) in visited: continue
         visited[(node[0], food)] = (parent, action)
-
-        # Check if node is the goal node
         if p.isGoal(node):
-            print ('ucs number of nodes explored: ', count)
+            print('ucs number of nodes explored: ', count)
             return gCost, construct_path(node, visited)
-
         count += 1
-        # get neighbors
         neighbors = p.nextStates(node)
         for stepCost, n, a in neighbors:
             food = tuple(n[1])
-            newState = (stepCost+gCost, n, node, a)
+            newState = (stepCost + gCost, n, node, a)
             heapq.heappush(pq, newState)
     return None
 
-def AStar (p):
-    start = p.startState() # get the start state from the Problem
-    # define priority queue:
-    #   queue element: (heuristic, gCost, node, parent, action)
+def AStar(p):
+    start = p.startState()
     pq = [(p.h(start), 0, start, None, None)]
-    
     visited = {}
-    count = 0 # counter to count the number of nodes explored.
+    count = 0
     while pq:
-        # pop one node from the priority queue
         fCost, gCost, node, parent, action = heapq.heappop(pq)
-
-        # check if the node is in visited
         food = tuple(node[1])
         if (node[0], food) in visited: continue
-        # Add node to visited
         visited[(node[0], food)] = (parent, action)
-        # check if the node is a goal node
-
         if p.isGoal(node):
-            print ('A* number of nodes explored: ', count)
-            # return total cost and path
+            print('A* number of nodes explored: ', count)
             return gCost, construct_path(node, visited)
-        
-        # count keeps track of the number of nodes explored.
         count += 1
-
-        # Complete one line of code here
-        # get next states or neighbor states by calling nextState
-        # from the FourCornerProblem passing in the node.
-        
-        
-        # loop on all neighbors to add them with their cost
-        # to the priority queue
+        neighbors = p.nextStates(node)  # Get neighbors for A* search
         for step_cost, neighbor, a in neighbors:
-            f = gCost + step_cost + p.h(neighbor)
-            # Add nodes to priority queue
-            newState= (f, gCost+step_cost, neighbor, node, a)
-            # complete one line of code here
-            # insert newState to the priority queue pq
-            
+            f = gCost + step_cost + p.h(neighbor)  # f = g + h
+            newState = (f, gCost + step_cost, neighbor, node, a)
+            heapq.heappush(pq, newState)  # Add to priority queue
     return None, None
 
 filename = 'tinyCorners.txt'
-
-# Complete your code here:
-# Get an instance of the problem:
-
 
 # -------------------------------------------------------
 # BFS:
 # -------------------------------------------------------
 
-
- 
-# record start time
+p = Problem(filename)  # Instance of the problem
 startTime = time.time()
 plan = bfs(p)
 endTime = time.time()
-print (plan)
-print ('plan length:', len(plan))
-print ('Time: ', (endTime - startTime) * 10**3, "ms")
-print ('------------------------')
+print(plan)
+print('plan length:', len(plan))
+print('Time: ', (endTime - startTime) * 10**3, "ms")
+print('------------------------')
 
 pac = pacmanGraphic(1300, 700)
 pac.setup(p)
@@ -136,44 +103,35 @@ pac.runPlan(p, plan)
 # UCS:
 # -------------------------------------------------------
 
-p = Problem(filename)
+p = Problem(filename)  # New instance of the problem
 startTime = time.time()
-p.compute_distances()
+p.compute_distances()  # Precompute distances for UCS and A*
 cost, plan = ucs(p)
 endTime = time.time()
 print('cost: ', cost)
 print(plan)
 print('plan length=', len(plan))
-print ('Time: ', (endTime - startTime) * 10**3, "ms")
+print('Time: ', (endTime - startTime) * 10**3, "ms")
 
 pac = pacmanGraphic(1300, 700)
 pac.setup(p)
 pac.runPlan(p, plan)
 
 # -------------------------------------------------------
-# A*
+# A*:
 # -------------------------------------------------------
 
-# Complete one line of code here:
-# get an instance of the Problem passing
-#    in the file name
-
+p = Problem(filename)  # New instance of the problem for A*
+p.compute_distances()  # Precompute distances for A*
 startTime = time.time()
-# Complete two lines of code here
-# 1) call compute distances method in FourCornersProblem
-
-# 2) Call Astar passing in the instance of the Problem,
-#    it returns the cost and the plan similar to ucs
-
+cost, plan = AStar(p)  # Call A* with the problem instance
 endTime = time.time()
-print ('Time: ', (endTime - startTime) * 10**3, "ms")
-# 3) print the cost, the plan and the plan length
 print('cost=', cost)
 print(plan)
 print('plan length=', len(plan))
+print('Time: ', (endTime - startTime) * 10**3, "ms")
 
-# Leave this code for plan execute and
-# moves pacman to collect the dots.
+# Execute the plan with graphics
 pac = pacmanGraphic(1300, 700)
 pac.setup(p)
 pac.runPlan(p, plan)
